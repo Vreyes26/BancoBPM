c56a50c30f90b58f636407bf7e38983bbd04bc93
Vreyes26 <Vreyes26@users.noreply.github.com>
2020-10-31 17:27:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/bancobpm/Info_act.java
@@ -0,0 +1,28 @@
+package com.example.bancobpm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Info_act extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_act);
+
+        videoView = (VideoView)findViewById(R.id.vd);
+
+        String ruta = "android.resource://"+getPackageName()+"/"+R.raw.video;
+        Uri uri = Uri.parse(ruta);
+        videoView.setVideoURI(uri);
+
+        MediaController media = new MediaController(this);
+        videoView.setMediaController(media);
+    }
+}
---
+++ app/src/main/java/com/example/bancobpm/Maps_act.java
@@ -0,0 +1,14 @@
+package com.example.bancobpm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Maps_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_act);
+    }
+}
